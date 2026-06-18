0a9b0ee68c080bc47ac61b5ac10d5e419930a616
Maryam Joumma <joummamaryam9@gmail.com>
2026-06-19 01:22:35 +0200

    Add initial JSON API endpoint

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+
 app = Flask(__name__)
+
 @app.route("/")
-
 def home():
-
     return render_template("index.html")
 
-if __name__ == "__main__":
 
+@app.route("/api/items")
+def api_items():
+    items = [
+        {
+            "id": 1,
+            "name": "Bohrmaschine",
+            "status": "available"
+        },
+        {
+            "id": 2,
+            "name": "Leiter",
+            "status": "available"
+        }
+    ]
+
+    return jsonify(items)
+
+
+if __name__ == "__main__":
     app.run(debug=True)

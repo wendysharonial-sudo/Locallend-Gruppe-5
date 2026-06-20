515b72f8e71db585c251c6d657e39413634b9ae4
Tuba Celik <tubaclk@MacBook-Air-von-Tuba.local>
2026-06-20 14:59:22 +0200

    Add login aqnd register backend

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
-from database.models import db
+from flask import Flask, render_template, request, redirect, url_for, jsonify, session
+from werkzeug.security import generate_password_hash, check_password_hash
+from database.models import db, User
 
 app = Flask(__name__)
 
+app.secret_key = "locallend-security-key"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///locallend.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
@@ -46,6 +48,79 @@ requests = [
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+
+    if request.method == "POST":
+
+        first_name = request.form["first_name"]
+
+        last_name = request.form["last_name"]
+
+        email = request.form["email"]
+
+        password = request.form["password"]
+
+        existing_user = User.query.filter_by(email=email).first()
+
+        if existing_user:
+
+            return "Email already registered."
+
+        hashed_password = generate_password_hash(password)
+
+        new_user = User(
+
+            first_name=first_name,
+
+            last_name=last_name,
+
+            email=email,
+
+            password=hashed_password
+
+        )
+
+        db.session.add(new_user)
+
+        db.session.commit()
+
+        return redirect(url_for("login"))
+
+    return render_template("register.html")
+
+@app.route("/login", methods=["GET", "POST"])
+
+def login():
+
+    if request.method == "POST":
+
+        email = request.form["email"]
+
+        password = request.form["password"]
+
+        user = User.query.filter_by(email=email).first()
+
+        if user and check_password_hash(user.password, password):
+
+            session["user_id"] = user.user_id
+
+            session["user_name"] = user.first_name
+
+            return redirect(url_for("home"))
+
+        return "Login failed."
+
+    return render_template("login.html")
+
+@app.route("/logout")
+
+def logout():
+
+    session.clear()
+
+    return redirect(url_for("home"))
 
 
 @app.route("/browse")

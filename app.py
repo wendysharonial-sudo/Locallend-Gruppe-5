becb6c5439b315b13383335d674522a20b0175a8
Wendy Lontsi DOumtsop <wendysharonial@gmail.com>
2026-06-21 22:13:22 +0200

    Working version before SQLAlchemy update

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
-from database.models import db
+from database.models import db, User, Item, Request as BorrowRequest
 
 app = Flask(__name__)
 
@@ -9,6 +9,48 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 with app.app_context():
     db.create_all()
+
+    if User.query.count() == 0:
+        user1 = User(
+        first_name="Anna",
+        last_name="Müller",
+        email="anna@example.com",
+        password="test123"
+
+        )
+
+        user2 = User(
+        first_name="Max",
+        last_name="Soleil",
+        email="max@example.com",
+        password="test123"
+
+
+        )
+
+        db.session.add_all([user1,user2])
+        db.session.commit()
+
+        item1 = Item(
+        user_id= user1.user_id,
+        title="Bohrmachine",
+        category= "Werkzeug",
+        description= "Für kleine Reperaturen und Umzüge",
+        availability= "available"
+        )
+
+        item2 = Item(
+        user_id= user2.user_id,
+        title= "Beamer",
+        category="Technik",
+        description="Für Präsentationen oder Filmabende",
+        availability="available"
+        )
+
+        db.session.add_all([item1, item2])
+        db.session.commit()
+
+
 
 items = [
     {
@@ -50,62 +92,81 @@ def home():
 
 @app.route("/browse")
 def browse():
+    items = Item.query.all()
     return render_template("browse.html", items=items)
 
 
 @app.route("/item/<int:item_id>")
 def item_detail(item_id):
-    selected_item = None
-
-    for item in items:
-        if item["id"] == item_id:
-            selected_item = item
-
+    selected_item = Item.query.get_or_404(item_id)
     return render_template("item_detail.html", item=selected_item)
 
 
 @app.route("/create-item", methods=["GET", "POST"])
 def create_item():
-    if request.method == "POST":
-        new_item = {
-            "id": len(items) + 1,
-            "title": request.form["title"],
-            "category": request.form["category"],
-            "description": request.form["description"],
-            "status": "available"
-        }
 
-        items.append(new_item)
+    if request.method == "POST":
+        new_item = Item(
+            user_id=1,
+            title=request.form["title"],
+            category=request.form["category"],
+            description=request.form["description"],
+            availability="available"
+        )
+
+        db.session.add(new_item)
+        db.session.commit()
+
         return redirect(url_for("browse"))
 
     return render_template("create_item.html")
 
 @app.route("/requests")
 def requests_page():
+    requests = BorrowRequest.query.all()
     return render_template("requests.html", requests=requests)
 
 
 @app.route("/requests/<int:request_id>/accept")
 def accept_request(request_id):
-    for request_item in requests:
-        if request_item["id"] == request_id:
-            request_item["status"] = "accepted"
+   
+    request_item = BorrowRequest.query.get_or_404(request_id)
+
+    request_item.status = "accepted"
+
+    db.session.commit()
 
     return redirect(url_for("requests_page"))
 
 
 @app.route("/requests/<int:request_id>/reject")
 def reject_request(request_id):
-    for request_item in requests:
-        if request_item["id"] == request_id:
-            request_item["status"] = "rejected"
+    
+    request_item = BorrowRequest.query.get_or_404(request_id)
+
+    request_item.status = "rejected"
+
+    db.session.commit()
 
     return redirect(url_for("requests_page"))
 
 
 @app.route("/api/items")
 def api_items():
-    return jsonify(items)
+    items = Item.query.all()
 
-if __name__ == "__main__":
+    items_data = []
+
+    for item in items:
+        items_data.append({
+            "id": item.item_id,
+            "title": item.title,
+            "category": item.category,
+            "description": item.description,
+            "status": item.availability
+        })
+
+    return jsonify(items_data)
+
+if __name__== "__main__":
     app.run(debug=True)
